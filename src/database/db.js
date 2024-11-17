fe0680280660201beeb6cf94f3393f72a9f3d201
WILLOSU <WILLOSU>
2024-11-17 18:22:34 -0300

    Remove useUnifiedTopology deprecated option and improve logging

--- src/database/db.js
+++ src/database/db.js
@@ -1,16 +1,14 @@
 import mongoose from "mongoose";
 
 const connectDatabase = () => {
-  console.log("wait conecting to the database");
+  console.log("Wait, connecting to the database...");
 
   mongoose
     .connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useNewUrlParser: true, // Mantém esta opção, pois ainda é relevante
     })
     .then(() => console.log("MongoDB connected"))
-    .catch((error) => console.log(error));
+    .catch((error) => console.error("Database connection error:", error));
 };
 
 export default connectDatabase;
-
